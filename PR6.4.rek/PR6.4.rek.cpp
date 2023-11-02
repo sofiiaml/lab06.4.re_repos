59b315d3336ad60244bdbb0422704537dea2e73c
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-11-02 22:02:54 +0200

    pr6.4.re

---
+++ PR6.4.rek/PR6.4.rek.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+using namespace std;
+
+int CountPositiveElements(double* a, int n, int i) {
+    if (i == n) {
+        return 0;
+    }
+    int count = CountPositiveElements(a, n, i + 1);
+    if (a[i] > 0) {
+        count++; 
+    }
+    return count;
+}
+
+double SumAfterLast0(double* a, int n, int i = 0, double lastSum = 0) {
+    if (i == n) {
+        return lastSum;
+    }
+    if (a[i] == 0) {
+        return SumAfterLast0(a, n, i + 1, 0); 
+    }
+    return SumAfterLast0(a, n, i + 1, lastSum + a[i]);
+}
+
+void ChangeArray(double* a, int n, int currentIndex) {
+    if (currentIndex == n) {
+        return; 
+    }
+    if (a[currentIndex] <= 1.0) {
+        double temp = a[currentIndex];
+        a[currentIndex] = a[0];
+        a[0] = temp;
+    }
+    ChangeArray(a, n, currentIndex + 1);
+}
+
+int main() {
+    int n;
+    cout << "n= "; cin >> n;
+
+    double* a = new double[n];
+
+    cout << "n" << n << " =" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int count = CountPositiveElements(a, n, 0);
+    cout << "Amount of positive elements=" << count << endl;
+    double sum = SumAfterLast0(a, n);
+    cout << "Sum=" << sum << endl;
+    ChangeArray(a, n, 0);
+    cout << "Changed array=" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+
+    delete[] a;
+
+    return 0;
+}
---
+++ UnitTest6.4.re/UnitTest6.4.re.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR6.4.rek/PR6.4.rek.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest64re
+{
+	TEST_CLASS(UnitTest64re)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int n = 4;
+			double a[n] = { 3.0, 1.0, 5.0, -2.0 };
+			int count=CountPositiveElements(a, n,0);
+			Assert::AreEqual(count,3);
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			const int n= 5;
+			double a[n] = { 3.0 ,6.0 ,0 ,4.0 ,2.0 };
+			double sum = SumAfterLast0(a, n,0,0);
+			Assert::AreEqual(sum,6,0.0000001);
+		}
+	};
+}
